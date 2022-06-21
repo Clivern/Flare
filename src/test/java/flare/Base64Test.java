3082d7d9ecca1f8cb5a158b9c40725de1f0f902f
Clivern <hello@clivern.com>
2022-06-21 23:27:34 +0200

    chore: port spark PR-1257, pr-1239, pr-1211, pr-1207

--- src/test/java/flare/Base64Test.java
+++ src/test/java/flare/Base64Test.java
@@ -7,16 +7,12 @@ import static org.junit.Assert.*;
 
 public class Base64Test {
 
-    //CS304 manually Issue link:https://github.com/perwendel/flare/issues/1061
-
     @Test
     public final void test_encode() {
         String in = "hello";
         String encode = Base64.encode(in);
         Assert.assertFalse(in.equals(encode));
     }
-
-    //CS304 manually Issue link:https://github.com/perwendel/flare/issues/1061
 
     @Test
     public final void test_decode() {
@@ -27,4 +23,17 @@ public class Base64Test {
         Assert.assertTrue(in.equals(decode));
     }
 
+    @Test
+    public final void testEncodeNull() {
+        String in = null;
+        String encode = Base64.encode(in);
+        Assert.assertNull(encode);
+    }
+
+    @Test
+    public final void testDecodeNull() {
+        String in = null;
+        String decode = Base64.decode(in);
+        Assert.assertNull(decode);
+    }
 }
